f43e08e4c583046ce0a5f0f0d11fe0eed522844a
Hakeem <emuy84@gmail.com>
2024-07-24 14:39:38 +0800

    Completed the basics

---
+++ index.js
@@ -0,0 +1,24 @@
+let buttonMinus = document.getElementById("buttonMinus-el")
+let buttonPlus = document.getElementById("buttonPlus-el")
+let displayEl = document.getElementById("display-el")
+let passwordEl = document.getElementById("password-el")
+
+let smallLetters = [
+                    'a', 'b', 'c', 'd', 'e', 'f', 
+                    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+                    'w', 'x', 'y', 'z'
+                     ]
+let capitalLetters = [
+                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+                    'W', 'X', 'Y', 'Z'
+                    ]
+let specialCharacters = [
+                    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+', 
+                    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+                ]
+
+
+
+   
