aa64d81e44aae2b4d583744836c7fae180ad0ad2
Hakeem <emuy84@gmail.com>
2024-07-24 17:21:43 +0800

    Javascript finished, html css polishing

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
 let displayEl = document.getElementById("display-el")
 let passwordEl = document.getElementById("password-el")
-let password = []
-passwordEl.textContent = ""
+
 let smallLetters = [
                     'a', 'b', 'c', 'd', 'e', 'f', 
                     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
                     'w', 'x', 'y', 'z'
-                     ]
+                ]
 let capitalLetters = [
                     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
                     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
                     'W', 'X', 'Y', 'Z'
-                    ]
+                ]
 let specialCharacters = [
                     '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+', 
                     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
                 ]
 
+let length = 1
+displayEl.textContent = length
 
-function random(){
-    let randomSmallLetter = Math.floor(Math.random() * 26 ) 
-    console.log(smallLetters[randomSmallLetter]) 
+function add(){
+    length++
+    displayEl.textContent = length
 }
-random()
+function decrease(){
+    length--
+    displayEl.textContent = length 
+}
+
+
+
+function generate() {
+
+    passwordEl.textContent = ''
+    for (let i = 0; i < length; i++){
+        let random = Math.floor(Math.random() * 26)
+        passwordEl.textContent += smallLetters[random]
+    }
+    for (let i = 0; i < length; i++){
+        let random = Math.floor(Math.random() * 26)
+        passwordEl.textContent += capitalLetters[random]
+    }
+    for (let i = 0; i < length; i++){
+        let random = Math.floor(Math.random() * 23)
+        passwordEl.textContent += specialCharacters[random]
+    }
+    for (let i = 0; i < length; i++){
+        let random = Math.floor(Math.random() * 26)
+        passwordEl.textContent += capitalLetters[random]
+    }
+    for (let i = 0; i < length; i++){
+        let random = Math.floor(Math.random() * 26)
+        passwordEl.textContent += smallLetters[random]
+    }
+}
+
+
+
 
 
 
