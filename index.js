35ddacd9e4fb6e6f363da17768fef6e2c4067c8e
Hakeem <emuy84@gmail.com>
2024-07-24 14:56:49 +0800

    fixed js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let buttonMinus = document.getElementById("buttonMinus-el")
-let buttonPlus = document.getElementById("buttonPlus-el")
 let displayEl = document.getElementById("display-el")
 let passwordEl = document.getElementById("password-el")
-
+let password = []
+passwordEl.textContent = ""
 let smallLetters = [
                     'a', 'b', 'c', 'd', 'e', 'f', 
                     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
@@ -18,6 +17,14 @@ let specialCharacters = [
                     '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+', 
                     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
                 ]
+
+
+function random(){
+    let randomSmallLetter = Math.floor(Math.random() * 26 ) 
+    console.log(smallLetters[randomSmallLetter]) 
+}
+random()
+
 
 
 
